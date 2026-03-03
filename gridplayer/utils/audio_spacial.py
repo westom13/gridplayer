71f4e310994166ac21739fbf3b9bd05404388609
Tom <westom13@gmail.com>
2026-03-03 21:59:00 +0100

    fix

--- gridplayer/utils/audio_spacial.py
+++ gridplayer/utils/audio_spacial.py
@@ -3,24 +3,26 @@ from gridplayer.vlc_player.libvlc import vlc
 def apply_spatial_audio(player, column_index, total_columns,
                         max_volume=100, min_volume=70):
 
+    active_player = player._playlist_player.get_media_player()
+
     if total_columns <= 1:
-        player.audio_set_channel(vlc.AudioOutputChannel.Stereo)
-        player.audio_set_volume(max_volume)
+        active_player.audio_set_channel(vlc.AudioOutputChannel.Stereo)
+        active_player.audio_set_volume(max_volume)
         return
 
     # ---- LEFT / RIGHT CHANNEL SPLIT ----
     midpoint = (total_columns - 1) / 2
 
     if column_index < midpoint:
-        player.audio_set_channel(vlc.AudioOutputChannel.Left)
+        active_player.audio_set_channel(vlc.AudioOutputChannel.Left)
     elif column_index > midpoint:
-        player.audio_set_channel(vlc.AudioOutputChannel.Right)
+        active_player.audio_set_channel(vlc.AudioOutputChannel.Right)
     else:
-        player.audio_set_channel(vlc.AudioOutputChannel.Stereo)
+        active_player.audio_set_channel(vlc.AudioOutputChannel.Stereo)
 
     # ---- VOLUME DIMMING ----
     distance_from_center = abs(column_index - midpoint) / midpoint
     volume_range = max_volume - min_volume
     volume = max_volume - (distance_from_center * volume_range)
 
-    player.audio_set_volume(int(volume))
+    active_player.audio_set_volume(int(volume))
